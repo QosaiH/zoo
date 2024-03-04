dbaa2416e600e108aaf3688ed3d8eb94b93d07ff
QosaiH <qosaihardan@gmail.com>
2024-03-04 13:12:31 +0200

    man and women by url

--- login.js
+++ login.js
@@ -37,9 +37,9 @@ function createCardHTML(visitors) {
   let cardHTML = "";
   visitors.forEach((visitor) => {
     if (visitor.gender == "male") {
-      cardHTML += `<div class="card"><img class="card-img-top" src="../../photos/man.png" alt="${visitor.name}" /><div class="card-body"><h3 class="card-title">${visitor.name}</h3><p class="card-text">Coins: ${visitor.coins}</p><button class="btn btn-primary" onclick="loginAsVisitor('${visitor.name}')">Login</button></div></div>`;
+      cardHTML += `<div class="card"><img class="card-img-top" src="https://www.flaticon.com/download/icon/4140048?icon_id=4140048&author=250&team=250&keyword=Man&pack=4139947&style=1&style_id=133&format=png&color=%23000000&colored=2&size=512&selection=1&type=standard&search=man" alt="${visitor.name}" /><div class="card-body"><h3 class="card-title">${visitor.name}</h3><p class="card-text">Coins: ${visitor.coins}</p><button class="btn btn-primary" onclick="loginAsVisitor('${visitor.name}')">Login</button></div></div>`;
     } else {
-      cardHTML += `<div class="card"><img class="card-img-top" src="../../photos/women.png" alt="${visitor.name}" /><div class="card-body"><h3 class="card-title">${visitor.name}</h3><p class="card-text">Coins: ${visitor.coins}</p><button class="btn btn-primary" onclick="loginAsVisitor('${visitor.name}')">Login</button></div></div>`;
+      cardHTML += `<div class="card"><img class="card-img-top" src="https://www.flaticon.com/download/icon/6997662?icon_id=6997662&author=670&team=670&keyword=Woman&pack=6997480&style=Circular&style_id=1216&format=png&color=%23000000&colored=2&size=512&selection=1&type=standard&search=women" alt="${visitor.name}" /><div class="card-body"><h3 class="card-title">${visitor.name}</h3><p class="card-text">Coins: ${visitor.coins}</p><button class="btn btn-primary" onclick="loginAsVisitor('${visitor.name}')">Login</button></div></div>`;
     }
   });
   return cardHTML;
